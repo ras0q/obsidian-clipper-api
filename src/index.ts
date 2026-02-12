92e0d9455a7d6a1483d80e7a2d4d7eca4c9ceffe
ras0q <ras0q@users.noreply.github.com>
2026-02-12 20:21:49 +0900

    refactor: simplify API

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,22 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
+import packageJSON from "../package.json";
 import convert from "./routes/convert.ts";
-import health from "./routes/health.ts";
 
 const app = new Hono();
 
 app.use("*", logger());
 app.use("*", cors());
 
-app.route("/health", health);
 app.route("/convert", convert);
 
 app.get("/", (c) => {
 	return c.json({
-		name: "Obsidian Clipper API",
-		version: "0.1.0",
+		name: packageJSON.name,
+		version: packageJSON.version,
+		description: packageJSON.description,
 		endpoints: {
-			health: "GET /health",
 			convert: "POST /convert",
 		},
 	});
